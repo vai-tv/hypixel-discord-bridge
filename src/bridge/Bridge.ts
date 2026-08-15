56a51dcf42a4cb9654651242e6b965d6e6760156
vaivai <138060923+novyne@users.noreply.github.com>
2026-08-15 20:49:13 +0100

    fix(bridge): split ChatMessage interface into separate Minecraft and Discord interfaces
    This is to resolve the mismatch between the two chat types in the respective managers.

--- src/bridge/Bridge.ts
+++ src/bridge/Bridge.ts
@@ -2,10 +2,16 @@ import { EventEmitter } from 'events';
 
 export type ChatChannel = 'guild' | 'officer' | 'debug';
 
-export interface ChatMessage {
+export interface MinecraftChatMessage {
   username: string;
   message: string;
   rank?: string;
+  channel: ChatChannel;
+}
+
+export interface DiscordChatMessage {
+  username: string;
+  message: string;
   channel: ChatChannel;
 }
 
@@ -14,11 +20,11 @@ export class Bridge extends EventEmitter {
     super();
   }
 
-  public emitMinecraftChat(data: ChatMessage): void {
+  public emitMinecraftChat(data: MinecraftChatMessage): void {
     this.emit('minecraftChat', data);
   }
 
-  public emitDiscordChat(data: ChatMessage): void {
+  public emitDiscordChat(data: DiscordChatMessage): void {
     this.emit('discordChat', data);
   }
 }
