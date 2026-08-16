abb282dbd3ce199bcb0d41153cce7a41edd8403b
vaivai <138060923+novyne@users.noreply.github.com>
2026-08-16 14:43:52 +0100

    refactor(api): include player guild in return of getPlayer

--- src/api/HypixelAPI.ts
+++ src/api/HypixelAPI.ts
@@ -1,9 +1,9 @@
 import { environment } from '../EnvHandler.js';
 import { Client } from 'hypixel-api-reborn';
-import type { Player } from 'hypixel-api-reborn';
+import type { Guild, Player } from 'hypixel-api-reborn';
 
 export class HypixelAPI {
-    private hypixel: Client;
+    public hypixel: Client;
     private apiKey: string;
 
     constructor() {
@@ -12,14 +12,17 @@ export class HypixelAPI {
       this.hypixel = new Client(this.apiKey, { cache: true });
     }
 
-    public async getPlayer(username: string): Promise<Player | null> {
+    public async getPlayer(username: string): Promise<{ player: Player, guild: Guild}  | null> {
         try {
             const mojangRes = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
             if (!mojangRes.ok) return null;
 
             const player = await this.hypixel.getPlayer(username);
             if (!player) return null;
-            return player;
+            
+            const guild = await this.hypixel.getGuild("player", username);
+            return { player, guild };
+
         } catch (error) {
             console.error(`[HypixelAPI] Error fetching player ${username}: ${error}`);
             return null;
