c77d3f7476688f2cda5f649d41868d3242902719
vaivai <138060923+novyne@users.noreply.github.com>
2026-08-16 11:33:01 +0100

    feat(discord): add chat acknowlegement events to Bridge
    Enables for Discord -> Minecraft timeout support in the next commit.

--- src/bridge/Bridge.ts
+++ src/bridge/Bridge.ts
@@ -3,28 +3,33 @@ import { EventEmitter } from 'events';
 export type ChatChannel = 'guild' | 'officer' | 'debug';
 
 export interface MinecraftChatMessage {
-  username: string;
-  message: string;
-  rank?: string;
-  channel: ChatChannel;
+    username: string;
+    message: string;
+    rank?: string;
+    channel: ChatChannel;
 }
 
 export interface DiscordChatMessage {
-  username: string;
-  message: string;
-  channel: ChatChannel;
+    id: string;
+    username: string;
+    message: string;
+    channel: ChatChannel;
 }
 
 export class Bridge extends EventEmitter {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  public emitMinecraftChat(data: MinecraftChatMessage): void {
-    this.emit('minecraftChat', data);
-  }
+    public emitMinecraftChat(data: MinecraftChatMessage): void {
+        this.emit('minecraftChat', data);
+    }
 
-  public emitDiscordChat(data: DiscordChatMessage): void {
-    this.emit('discordChat', data);
-  }
+    public emitDiscordChat(data: DiscordChatMessage): void {
+        this.emit('discordChat', data);
+    }
+
+    public emitDiscordChatAck(messageId: string): void {
+        this.emit('discordChatAck', messageId);
+    }
 }
