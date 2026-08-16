25290eaec773361585fcd498f36dbc5bbf5c0d7d
vaivai <138060923+novyne@users.noreply.github.com>
2026-08-16 14:23:12 +0100

    perf(api): add a Mojang API call first to verify the player before making the Hypixel call
    This should help prevent ratelimiting.

--- src/api/HypixelAPI.ts
+++ src/api/HypixelAPI.ts
@@ -1,16 +1,21 @@
+import { environment } from '../EnvHandler.js';
 import { Client, Player } from 'hypixel-api-reborn';
 
 export class HypixelAPI {
     private hypixel: Client;
+    private apiKey: string;
 
     constructor() {
-        const apiKey = process.env.HYPIXEL_API_KEY;
-        if (!apiKey) throw new Error('[HypixelAPI] Missing Hypixel API key in .env!');
-        this.hypixel = new Client(apiKey);
+      this.apiKey = environment.minecraft.hypixelApiKey || '';
+      if (!this.apiKey) throw new Error('Missing Hypixel API key in .env!');
+      this.hypixel = new Client(this.apiKey);
     }
 
     public async getPlayer(username: string): Promise<Player | null> {
         try {
+            const mojangRes = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+            if (!mojangRes.ok) return null;
+
             const player = await this.hypixel.getPlayer(username);
             if (!player) return null;
             return player;
