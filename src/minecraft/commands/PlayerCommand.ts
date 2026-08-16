dc1168dcfda2b631c446f444d478c5057c27d3c9
vaivai <138060923+novyne@users.noreply.github.com>
2026-08-16 14:43:05 +0100

    feat(command): complete PlayerCommand and refactor according to format

--- src/minecraft/commands/PlayerCommand.ts
+++ src/minecraft/commands/PlayerCommand.ts
@@ -2,22 +2,36 @@ import { MinecraftCommand } from "../../contracts/Command.js";
 import type { MinecraftCommandContext } from "../../contracts/Command.js";
 import { HypixelAPI } from "../../api/HypixelAPI.js";
 
-export class StatsCommand extends MinecraftCommand {
+export class PlayerCommand extends MinecraftCommand {
+    private hypixelApi: HypixelAPI;
+
     public name = 'player';
     public description = 'Get player overall stats';
     public aliases = ['me', 'p'];
 
-    private hypixelApi: HypixelAPI;
-
     constructor(hypixelApi: HypixelAPI) {
         super();
-        this.hypixelApi = new HypixelAPI();
+        this.hypixelApi = hypixelApi;
     }
 
     public async execute(context: MinecraftCommandContext): Promise<void> {
         const target = context.args[0] || context.username;
-        const player = await this.hypixelApi.getPlayer(target);
 
-        await context.reply('Unimplemented!');
+        try {
+            const { player, guild } = await this.hypixelApi.getPlayer(target) || {};
+
+            if (!player) {
+                await context.reply(`Player '${target}' not found or API request failed.`);
+                return;
+            }
+
+            const { level, rank, nickname, karma } = player;
+
+            await context.reply(`(${level}) ${rank} ${nickname} :  member of ${guild} | ${karma.toLocaleString()} Karma`);
+
+        } catch (err: any) {
+            console.error(`[MINECRAFT Command] Error:`, err);
+            await context.reply(`[ERROR]: ${err}`);
+        }
     }
 }
